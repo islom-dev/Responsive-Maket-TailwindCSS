2c889d59c6293671530e8783ae6530d56ad8e2c4
Saidov Islom <saidovislom7777@gmail.com>
2024-07-03 16:35:17 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "320px", 
+        // => @media (min-width: 640px) { ... }
+  
+        md: "768px",
+        // => @media (min-width: 208px) (max-width:768) { ... }
+  
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+  
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+  
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
+    
+  },
+  plugins: [],
+
+}
+
